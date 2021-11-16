6ff20d6bd7f3b3238f0416b6db742ba7f91ef19a
ahmediftiar <iamiftiar@gmail.com>
2021-11-16 06:38:01 +0600

    first commit

---
+++ firstGolang.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numnerOne string = "The name Is"
+	var numnerTwo string = "Iftiar"
+	numberThree := "Ahmed"
+	fmt.Println(numnerOne, numnerTwo, numberThree)
+}
